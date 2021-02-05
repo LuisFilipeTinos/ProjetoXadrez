744faaf9189f273d0327bed295e016a4e40eb782
Luis Filipe Tinos <lftinos@gmail.com>
2021-02-05 00:41:05 -0300

    Comecando a implementar o tabuleiro

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace ProjetoXadrez
 {
@@ -7,9 +7,7 @@ namespace ProjetoXadrez
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
---
+++ ProjetoXadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+
+        public Posicao PosicaoPeca { get; set; }
+        public Cor CorPeca { get; protected set; }
+        public int QtdDeMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicaoPeca, Cor corPeca, Tabuleiro tab)
+        {
+            this.PosicaoPeca = posicaoPeca;
+            this.CorPeca = corPeca;
+            this.Tab = tab;
+            this.QtdDeMovimentos = 0;
+        }
+    }
+}
